4cce84ae97637c46e87c8111a4e52c38b06380e6
Daniel Cesario <danielcesario@outlook.com>
2023-06-07 23:19:30 -0300

    Add Crete Transaction Handler

--- cmd/api/handler/transaction.go
+++ cmd/api/handler/transaction.go
@@ -1,6 +1,15 @@
 package handler
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/danielcesario/cdc-core/internal/transaction"
+	"github.com/gin-gonic/gin"
+)
+
 type TransactionService interface {
+	Create(ctx context.Context, request transaction.TransactionRequest) (*transaction.TransactionResponse, error)
 }
 
 type TransactionHandler struct {
@@ -11,4 +20,22 @@ func NewTransactionHandler(service TransactionService) *TransactionHandler {
 	return &TransactionHandler{
 		service: service,
 	}
+}
+
+func (h *Handler) CreateTransaction(context *gin.Context) {
+	var request transaction.TransactionRequest
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+
+	response, err := h.transaction.service.Create(context, request)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+
+	context.JSON(http.StatusCreated, response)
 }
